f821dfb5d1e34c9a6052e0b68962edac255df015
Glitch (tinted-vessel) <none>
2020-01-19 14:00:48 +0000

    🍋🚎 Checkpoint ./ayarlar.json:3054458/77 ./config.js:3054458/177

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-  "token": "NjQzMTI3MTg2MjMyMTgwNzU3.XiRb8g.xgl8CtjFYHhZiKw0oMBO5BeGntM",
+  "token": "",
 
   "dashboard" : {
-    "oauthSecret": "643127186232180757", 
-    "callbackURL": "https://pie-fuchsia.glitch.me/callback",
-    "sessionSecret": "_78-HSf2ZFJ_00un4Tl62bvcUymZAlKK",
-    "domain": "https://pie-fuchsia.glitch.me/",
+    "oauthSecret": "", 
+    "callbackURL": "",
+    "sessionSecret": "",
+    "domain": "",
     "port": 8000
   },
 };
