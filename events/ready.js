c727e9edb72725fb564949406d37bd5c8636b1bc
Glitch (tinted-vessel) <none>
2020-02-16 18:40:49 +0000

    💋🌂 Checkpoint ./komutlar/csg.js:9624236/980 ./events/ready.js:9624236/43

--- events/ready.js
+++ events/ready.js
@@ -14,7 +14,7 @@ module.exports = async client => {
   
   console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("oline");
-  client.user.setGame(`🔥a!yardım🔥a!koruma 🔥 a!sunucutanıt 🔥BETA  0.0.2 `);
+  client.user.setGame(`🔥a!yardım🔥a!koruma 🔥 a!premium 🔥 a!sunucutanıt 🔥BETA  0.0.4 `);
   console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
   require("../util/dashboard.js")(client);
 };
