e4e8ae192ee6a32d00e736687a5a2005ab0e5ead
Glitch (tinted-vessel) <none>
2020-02-16 17:54:17 +0000

    🏫🤗 Checkpoint ./komutlar/yardım.js:9624236/15 ./komutlar/pre.js:9624236/1273

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
       .addField('** Komutlar (13)**', `herkesin kullanabileceği standart komutlar. \n` +  '`a!bilgi`, `a!davet`, `a!gold-üye`, `a!webpanel`, `a!sunucubilgi`, `a!kullanıcı-bilgi`, `a!balıktut`, `a!seviye`, `a!yardım`, `a!erkek`, `yakında`, `yakında`,`a!yılbaşı`')
       .addField('** Eğlence (9)**',   `herkes için kullanılabilecek eğlence komutları. \n` + '`a!kralol`, `yakında`, `yakında`, `a!adamol`, `yakında`, `yakında`, `yakında`, `yakında`')
       .addField('** Moderasyon (22)**',`yetkililer için moderasyon komutları bölüm . \n` +  '`a!küfür-engelle`, `a!reklam-engel`, `a!reklamisimban`, `a!reklamkick`,`a!reklamtaraması`,`a!otorol-yardım`,`a!ototag`,`a!sayaç`,`a!sayaç-ayarla`,`a!sil`,`a!ultra-sohbet-temizleyici`,`a!eklam-engelleme` ,`a!koruma-sistemi` ,`a!kayıt-kanal-ayarla`,`a!hg-kanal`,`a!güvenlik`,`a!giriş-izni`,`a!`,`a!yakında`,`a!davet-takip-kanal`,')
-      .addField('** Moderasyon2 (13)**',`yetkililer için moderasyon komutları bölüm . \n` + '`a!bb-kanal`,`a!ayar`,`a!`,`a!capslock-filtresi`,`a!sunucutanıt`,`a!yavaşmod`,`a!prefix`,`a!Oto-Cevap`,`a!koruma`,`a!yakında`,`a!yakında`,`a!yakında`,`a!emojiler`')
+      .addField('** Moderasyon2 (13)**',`yetkililer için moderasyon komutları bölüm . \n` + '`a!bb-kanal`,`a!ayar`,`a!duyur`,`a!capslock-filtresi`,`a!sunucutanıt`,`a!yavaşmod`,`a!prefix`,`a!Oto-Cevap`,`a!koruma`,`a!yakında`,`a!yakında`,`a!yakında`,`a!emojiler`')
     .setFooter(`© ${client.user.username} ` , client.user.avatarURL)
     .setTimestamp()
     message.channel.send(embedyardim).catch()
