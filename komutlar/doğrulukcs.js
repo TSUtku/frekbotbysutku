142d2ed3481fe94c7402aed8f5689885ea86ebf1
Glitch (mcsda) <none>
2020-03-14 18:44:50 +0000

    🍉🎰 Checkpoint ./komutlar/yardım.js:9624236/85 ./komutlar/doğrulukcs.js:9624236/655

--- komutlar/doğrulukcs.js
+++ komutlar/doğrulukcs.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
-    var dogruluk = ['Telefonunda arattığın en son şey nedir ?', 'Bir sabah karşı cins olarak uyansaydın ilk yapacağın şey ne olurdu ?', 'Sokakta yere bir şey düşürdüğünde hiç bir şey olmamış gibi alıp ağzına attın mı ?', 'Merve ile ilgili hiisetiğin bir kelime söyle', 'Sevidğin kızın ismi', '**BOŞ ŞANSLI ÇOÇUK**', 'Hiç Öpüştünmü', 'Hiç seviştinmi', 'Botu nasıl buldun']
-    var cesaret = ['İğrenç bir ses tonuyla şarkı söyle', 'Bugün yaptığın bir şeyle alakalı uydurma kısa, komik bir hikaye anlat', 'Whatsappındaki son mesajlaşmanı bize oku', 'Çok yüksek bir sesle 3 saniye bağır', 'Durumuna MerveAkals Seviyorum yaz', 'BOŞ **ŞANSLISIN**', 'Merveye dm yaz Çıkma teklifi et', 'SUNUCUDA BİRİNE 10 SANİYELİĞİNE YETKİLİ YAP [tanımadığın biri]']
+    var dogruluk = ['Telefonunda arattığın en son şey nedir ?', 'Bir sabah karşı cins olarak uyansaydın ilk yapacağın şey ne olurdu ?', 'Sokakta yere bir şey düşürdüğünde hiç bir şey olmamış gibi alıp ağzına attın mı ?', 'Merve ile ilgili hiisetiğin bir kelime söyle', 'Sevidğin kızın ismi', '**BOŞ ŞANSLI ÇOÇUK**', 'Hiç Öpüştünmü', 'Hiç seviştinmi', 'Botu nasıl buldun', 'merve ile svişirmiydin', 'Merve güzelmi', 'en sevdğin film', 'en mutlu oldugun an', 'Merve sana çıkma teklifi etse kabul edermısın']
+    var cesaret = ['İğrenç bir ses tonuyla şarkı söyle', 'Bugün yaptığın bir şeyle alakalı uydurma kısa, komik bir hikaye anlat', 'Whatsappındaki son mesajlaşmanı bize oku', 'Çok yüksek bir sesle 3 saniye bağır', 'Durumuna MerveAkals Seviyorum yaz', 'BOŞ **ŞANSLISIN**', 'Merveye dm yaz Çıkma teklifi et', 'SUNUCUDA BİRİNE 10 SANİYELİĞİNE YETKİLİ YAP [tanımadığın biri]', 'merveyle +18 konuşma yap', 'Botu sunucunda tanıt', 'SUNUCUYA ÇAY İSMARLA', 'sunucudan hernagı birine hediye ver', 'BONUS**1 İNVİTE KAZANDIN**']
     var dogrulukcevap = dogruluk[Math.floor(Math.random() * dogruluk.length)];
     var cesaretcevap = cesaret[Math.floor(Math.random() * cesaret.length)]
 
