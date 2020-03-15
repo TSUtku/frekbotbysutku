840db0194db536021606cdd6b6cddd96c6e4a9e5
Glitch (mcsda) <none>
2020-03-15 12:30:47 +0000

    ☔️🍅 Checkpoint ./komutlar/tepkilirol.js:9624236/2513 ./bot.js:9624236/2626

--- bot.js
+++ bot.js
@@ -83,6 +83,73 @@ setInterval(() => {
   http.get(`http://mcsda.glitch.me/`);
 }, 280000);
 /////////////////////
+const yourID = "681872205193150488"; //Instructions on how to get this: https://redd.it/40zgse //Kendi İD'nizi Yazın
+const setupCMD = "a!tepkilirol" //İstediğiniz Komut Yapabilirsiniz örn : !kayıtol
+let initialMessage = `Test`; //Dilediğiniz Şeyi Yazabilirsiniz
+const roles = ["mavi", "Artist", "Public Relations", "Intern"]; //İstediğiniz Rolü Yazabilirsiniz
+const reactions = [":heart:", "", "", ""]; //İstediğiniz Emojiyi Ekleyebilirsiniz
+const botToken = "";  //Burası Boş Kalsın
+                    
+
+//Load up the bot...
+const bot = new Discord.Client();
+bot.login(botToken);
+
+//If there isn't a reaction for every role, scold the user!
+if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`Kayıt Olmak İçin **"${role}"** Emojisine Tıkla!`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]); 
+                }
+            });
+        }
+    }
+})
+
+
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+      
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != bot.user.id){
+                var roleObj = msg.guild.roles.find(r => r.name === role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.addRole(roleObj)
+                } else {
+                    memberObj.removeRole(roleObj);
+                }
+            }
+        }
+        })
+ 
+    }   
+});
+/////
 client.on("message", async msg => {
   if (msg.content.toLowerCase() === prefix + "disko") {
     msg.channel.send(
