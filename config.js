1d75f8f65535c7d40ec79cff42ac4cec995e664a
Glitch (mcsda) <none>
2020-03-16 09:18:49 +0000

    💁🍂 Checkpoint ./config.js:9624236/325

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const config = {
-  "token": "",
+  "token": "Njg4NjY2NzA4OTMxNjQxMzU5.Xm8lhQ.wFQWLhMxbf0Y5DPEP2RokZk3fhs",
+  "sahip": "681872205193150488",
 
   "dashboard" : {
     "oauthSecret": "", 
-    "callbackURL": "",
+    "callbackURL": "https://mcsda.glitch.me//callback",
     "sessionSecret": "",
-    "domain": "",
+    "domain": "https://mcsda.glitch.me/",
     "port": 8000
   },
 };
