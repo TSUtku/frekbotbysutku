4f52098244746d4d4566678de9fa49cd6dfb1ab0
Glitch (mcsda) <none>
2020-03-17 07:45:07 +0000

    🎺💺 Checkpoint ./komutlar/herkeserolver.js:9624236/1133 ./events/ready.js:9624236/6968

--- events/ready.js
+++ events/ready.js
@@ -6,16 +6,17 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 
-module.exports = async client => {
-    client.appInfo = await client.fetchApplication();
-    setInterval( async () => {
-      client.appInfo = await client.fetchApplication();
-    }, 60000);
-  
-  console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("oline");
-  client.user.setGame(`🔥a!yardım🔥a!koruma 🔥 YENİ KOMUT +15🔥 a!premium 🔥GOLD ÜYE TOP 10🔥BETA  0.0.11 `);
-  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-  require("../util/dashboard.js")(client);
+module.exports = client => {
+  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  //idle = boşta
+  //dnd = rahatsız etmeyin
+  //online = çevrimiçi
+  console.log(`${client.user.id}                                                                                                                                                                     `)
+  //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
+client.user.setActivity(`!BAKIMDA🔥 BETA 0.8.4`, { type: "WATCHING"});  
+//LISTENING = DİNLİYOR
+  //WATCHING = İZLİYOR
+  //PLAYING = OYNUYOR 
+  console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
-
