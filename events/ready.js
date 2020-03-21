dd4c8419d9ca4a95b4b29ba745c1b6734ca4fe57
Glitch (mcsda) <none>
2020-03-21 07:41:39 +0000

    😽🍵 Checkpoint ./ayarlar.json:9624236/118 ./events/ready.js:9624236/2156

--- events/ready.js
+++ events/ready.js
@@ -5,18 +5,11 @@ const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
 
-
 module.exports = client => {
-  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-  //idle = boşta
-  //dnd = rahatsız etmeyin
-  //online = çevrimiçi
-  console.log(`${client.user.id}                                                                                                                                                                     `)
-  //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
-client.user.setActivity(`🔥a!p-market🔥 BETA 0.8.4`, { type: "WATCHING"});  
-//LISTENING = DİNLİYOR
-  //WATCHING = İZLİYOR
-  //PLAYING = OYNUYOR 
-  console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("watching");
+  client.user.setGame(`🔥p-market🔥 🔥Beta 0.0.4🔥${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
