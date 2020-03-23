45e513c98df3b085352460cceb7f95ee60f21baf
Glitch (mcsda) <none>
2020-03-23 07:54:23 +0000

    🏁🌨 Checkpoint ./ayarlar.json:9624236/118 ./komutlar/p-menü.js:9624236/1715

--- komutlar/p-menü.js
+++ komutlar/p-menü.js
@@ -5,10 +5,10 @@ exports.run = async(client, message, args) => {
   if (!db.fetch(`goldpuan_${message.author.id}`)) {
     const Embed = new Discord.RichEmbed()
  .setTimestamp()
- .setAuthor("EŞŞEK-AT", client.user.avatarURL)
+ .setAuthor("MREASGLES", client.user.avatarURL)
 .setColor("BLUE")
-.setTitle("EŞŞEK-AT BOT")
- .setURL("https://discordapp.com/oauth2/authorize?client_id=647386467844227074&scope=bot&permissions=8")
+.setTitle("MREAGLES")
+ .setURL("https://discordapp.com/oauth2/authorize?client_id=688666708931641359&scope=bot&permissions=2146958847")
 
  .setDescription(`
 ${message.author}, senin puanın **0**
@@ -18,17 +18,38 @@ daha fazla puan kazanmak için !p-menü yazabilirsin.
 Puanınızı ve market menüsünü gösterir.
 
 `)
- .addField("!p-topaz", `
-2.500 puan karşılığında topaz kasa açarsınız.
-İçinden rastgele 1-2 gün arası gold üye çıkar.
+ .addField("a!p-topaz", `
+5.000 puan karşılığında topaz kasa açarsınız.
+İçinden rastgele 1-3 gün arası gold üye çıkar.
+`)
+ .addField("!ap-invite", `
+10.000 puan karşılığı invite kasa açarsınız.
+İçinden rastgele 1-10 arası invite çıkar.
+`)
+  .addField("a!p-ucuz", `
+1.000 puan karşılığı invite kasa açarsınız.
+İçinden rastgele 1-10 arası Vip  çıkar.
+`)
+   .addField("a!p-100k", `
+100000 puan karşılığı invite kasa açarsınız.
+İçinden rastgele 100-1000 arası GOLD ÜYE + 15 İNVİTE  çıkar.
+`)
+    .addField("a!p-yetki", `
+50000 puan karşılığı invite kasa açarsınız.
+İçinden rastgele Yetki puanı çıkar  çıkar.
+`)
+   .addField("a!p-sınırsız", `
+75000 puan karşılığı invite kasa açarsınız.
+İçinden rastgele SINIRISZ-SINIRSIZ arası Vip  çıkar.
 `)
  
- .addField("!p-yakut", `
+ 
+ .addField("a!p-yakut", `
 10.000 puan karşılığı yakut kasa açarsınız.
 İçinden rastgele 1-10 gün arası gold üye çıkar.
 `)
  
- .addField("!p-garnet", `
+ .addField("a!p-garnet", `
 45.000 puan karşılığı garnet kasa açarsınız.
 İçinden rastgele (10-45) gün arası gold üye çıkar.
 `)
