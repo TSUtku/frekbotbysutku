a08fd4314c754204d65fd2a66712d921a82dfbd2
Glitch (mcsda) <none>
2020-03-28 12:18:18 +0000

    🚧😬 Checkpoint ./bot.js:9624236/1143 ./ayarlar/spamEngel.json:9624236/3

--- bot.js
+++ bot.js
@@ -126,7 +126,31 @@ bot.on("message", message => {
         }
     }
 })
+///////////////////
+const antispam = require("discord-anti-spam-tr");
+client.on("message", msg => {
+  const spamEngel = db.get(`spamEngel_${msg.guild.id}`)
 
+  if(spamEngel == "açık"){
+
+let spamEngel = JSON.parse(fs.readFileSync("./ayarlar/spamEngel.json", "utf8"));
+//istediğiniz yere ekleyin bot.js de
+
+antispam(client, {
+  uyarmaSınırı: 15, //Uyarılmadan önce aralıkta gönderilmesine izin verilen maksimum mesaj miktarı.
+  banlamaSınırı: 20, //Yasaklanmadan önce aralıkta gönderilmesine izin verilen maksimum ileti miktar.
+  aralık: 300000, // ms kullanıcılarda zaman miktarı, yasaklanmadan önce aralık değişkeninin maksimumunu gönderebilir.
+  // Uyarı mesajı, kullanıcıya hızlı gideceklerini belirten kullanıcıya gönderilir..
+   //Yasak mesaj, yasaklanmış kullanıcıyı ,Banlar
+  maxSpamUyarı: 7,//Bir kullanıcının uyarılmadan önce bir zaman dilimi içinde gönderebileceği maksimum kopya sayısı
+  maxSpamBan: 20, //Bir kullanıcının yasaklanmadan önce bir zaman diliminde gönderebildiği maksimum kopya sayısı
+  zaman: 7, // Spamdan sonraki zaman
+  rolİsimi: "spam-susturulmuş" // Spam Atan Kullanıcılar Verilecek Röl
+})
+};
+    }
+)
+/////////////
 
 bot.on('raw', event => {
     if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
